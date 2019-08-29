d5cded56b5e218a0ea815e4f3f0cfadadb27c057
Steven Gu <steven.gu@letote.cn>
2019-08-29 15:23:47 +0800

    Implements `mmal` binding for first version.

---
+++ vendor/include/wrapper.h
@@ -0,0 +1,10 @@
+#include <bcm_host.h>
+#include <interface/vcos/vcos.h>
+
+#include <interface/mmal/mmal.h>
+#include <interface/mmal/mmal_buffer.h>
+#include <interface/mmal/mmal_parameters_camera.h>
+#include <interface/mmal/util/mmal_connection.h>
+#include <interface/mmal/util/mmal_default_components.h>
+#include <interface/mmal/util/mmal_util.h>
+#include <interface/mmal/util/mmal_util_params.h>
