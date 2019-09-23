a6c7631ffac8b3979c8660f7bba4ff28dd06539e
Steven <steven.gu@letote.cn>
2019-09-23 19:55:40 +0800

    Implements `mmal` binding for first version. (#1)

---
+++ build.rs
@@ -0,0 +1,31 @@
+extern crate bindgen;
+
+use std::env::var;
+use std::path::PathBuf;
+
+fn main() {
+    let bindings = bindgen::Builder::default()
+        .rustfmt_bindings(true)
+        .constified_enum_module("MMAL_STATUS_T")
+        .whitelist_type(r"MMAL_.*")
+        .whitelist_function(r"(?:bcm_|mmal_|vcos_).*")
+        .whitelist_var(r"MMAL_.*")
+        .derive_debug(true)
+        .impl_debug(true)
+        .clang_arg("-I/opt/vc/include")
+        .header("vendor/include/wrapper.h")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+
+    println!("cargo:rustc-link-lib=bcm_host");
+    println!("cargo:rustc-link-lib=mmal_core");
+    println!("cargo:rustc-link-lib=mmal_util");
+    println!("cargo:rustc-link-lib=mmal_vc_client");
+    println!("cargo:rustc-link-lib=vcos");
+    println!("cargo:rustc-link-search=native=/opt/vc/lib");
+}
