196c8a337f89a94987674937ad71f4769d39d545
Steven Gu <asongala@163.com>
2019-09-29 02:37:24 +0000

    Sets default target to `arm-linux-gnueabihf`.

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,17 @@
 #![allow(non_upper_case_globals)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+use std::os::raw::c_uint;
+
+#[macro_export]
+macro_rules! mmal_fourcc {
+    ($a:expr, $b:expr, $c:expr, $d:expr) => {
+        {
+            (($a as c_uint) | (($b as c_uint) << 8) | (($c as c_uint) << 16) | (($d as c_uint) << 24)) as c_uint
+        }
+    }
+}
+
+pub const MMAL_ENCODING_I420: c_uint = mmal_fourcc!('I','4','2','0');
+pub const MMAL_ENCODING_OPAQUE: c_uint = mmal_fourcc!('O','P','Q','V');
